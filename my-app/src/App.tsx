00bc8e16513d47b5b739ce0c84235e885a4906ac
bpat714 <38747233+bpat714@users.noreply.github.com>
2020-07-26 11:44:28 +1200

    testing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MSA Devops 2020
         </a>
       </header>
     </div>
